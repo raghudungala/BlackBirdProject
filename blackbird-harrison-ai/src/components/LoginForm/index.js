422cdb1677684826d1bf9e2d3cc1baddf6b0586a
Raghu Dungala <draghu222@gmail.com>
2023-06-06 01:44:24 -0500

    Task 2

---
+++ blackbird-harrison-ai/src/components/LoginForm/index.js
@@ -0,0 +1,129 @@
+import { useState } from 'react';
+import Alert from '@mui/material/Alert';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Snackbar from '@mui/material/Snackbar';
+import Typography from '@mui/material/Typography';
+import logo from '../../assets/logo.svg';
+import * as EmailValidator from 'email-validator';
+
+
+export default function LoginForm() {
+  const [showAlert, setShowAlert] = useState(false);
+  const [isEmailValidate, setIsEmailValidate] = useState(true);
+  const [isPasswordValidate, setIsPasswordValidate] = useState(true);
+  const validateForm = (event) => {
+    event.preventDefault()
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email');
+    const password = data.get('password');
+
+    // Add validation code here
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    const emailValidation = EmailValidator.validate(email);
+    const passwordValidation = passwordRegex.test(password);
+    setIsEmailValidate(emailValidation)
+    setIsPasswordValidate(passwordValidation)
+    return emailValidation && passwordValidation
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+    if(validateForm(event)){
+      setShowAlert("Login Successful");
+    }else{
+      setShowAlert("Login UnSuccessful");
+    }
+  };
+
+  return (
+    <>
+      {showAlert &&
+        <Snackbar
+          open={showAlert}
+          autoHideDuration={6000}
+          onClose={() => setShowAlert(false)}
+          message={showAlert}
+        >
+          <Alert severity={showAlert == "Login Successful"?"success":"error"}>{showAlert}</Alert>
+        </Snackbar>
+      }
+      <Grid
+        item
+        xs={false}
+        sm={4}
+        md={7}
+        sx={{
+          backgroundImage: 'url(https://source.unsplash.com/random)',
+          backgroundRepeat: 'no-repeat',
+          backgroundColor: (t) =>
+            t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+        }}
+      />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Box sx={{
+            my: 2
+          }}>
+            <img src={logo} width="147" alt="harrison.ai" />
+          </Box>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+            <TextField
+              error={!isEmailValidate}
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              helperText={!isEmailValidate && "Incorrect Email"}
+            />
+            <TextField
+              error={!isPasswordValidate}
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              helperText={!isPasswordValidate && "Incorrect Password"}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+    </>
+  );
+}
